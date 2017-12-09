06646eaa9092e1fc6c4c666b97e137ae501e88f9
Anh Dang <aqdang16@earlham.edu>
2017-12-09 17:40:34 -0500

    update

--- SpellChecking.hs
+++ SpellChecking.hs
@@ -1,32 +1,36 @@
 import Data.List
 import System.IO.Unsafe
+import Data.Char
+
 --Addition,Subtitution,Deletions
-addition :: [Char] -> [[Char]]
+addition :: String -> [String]
 addition word = [left ++ c: right | (left,right) <- splits word, c <- letters]
 	where letters = ['a'..'z']
 
-deletion :: [Char] -> [[Char]]
+deletion :: String -> [String]
 deletion word = [left ++ tail right | (left,right) <- splits word, (not . null) right]
 
-subtitution :: [Char] -> [[Char]]
+subtitution :: String -> [String]
 subtitution [] = []
 subtitution word = [left ++ c:tail right |(left,right) <- splits word, (not . null) right, c <- letters]
 	where letters = ['a'..'z']
 
-splits :: [Char] -> [([Char],[Char])]
+splits :: String -> [(String,String)]
 splits word = zip (inits word) (tails word)
 
-editOnce :: [Char] ->[[Char]]
+editOnce :: String ->[String]
 editOnce word = (addition word) ++ (deletion word) ++ (subtitution word)
 
 removePunc :: String -> String
 removePunc str = [x | x <- str, not (x `elem` punc)]
 	where punc = ",.?!:;\"\'#$%"
 
-internalDash :: String -> String
-internalDash str = [if (x == "-") || (x == "&") then " " else x | x <- str]
+internalHandle :: String -> String
+internalHandle str = [if (x == '-') || (x == '&') then ' ' else x | x <- str]
 
-
+wordNoLetters :: String -> Bool
+wordNoLetters "" = True
+wordNoLetters str = (not (isAlpha (head str))) && (wordNoLetters (tail str))
 
 --Import File into List
 getWord :: FilePath -> IO [String]
@@ -35,5 +39,5 @@ getWord path = do
 	return (lines contents)
 
 --SpellCheck
-sameElem :: [Char] -> FilePath -> [[Char]]
+sameElem :: String -> FilePath -> [String]
 sameElem word file = [x | x <- editOnce word, y <- unsafePerformIO $ getWord file , x == y ]
