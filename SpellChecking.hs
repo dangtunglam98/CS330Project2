4e50c91596bf65ad6ad8e1b7d372bf4639cdd4e4
Anh Dang <aqdang16@earlham.edu>
2017-12-09 15:02:34 -0500

    update

--- SpellChecking.hs
+++ SpellChecking.hs
@@ -21,10 +21,10 @@ editOnce word = (addition word) ++ (deletion word) ++ (subtitution word)
 
 removePunc :: String -> String
 removePunc str = [x | x <- str, not (x `elem` punc)]
-	where punc = ",.?!:;\"\'"
+	where punc = ",.?!:;\"\'#$%"
 
 internalDash :: String -> String
-internalDash str = [if x == "-" then " " else x | x <- str]
+internalDash str = [if (x == "-") || (x == "&") then " " else x | x <- str]
 
 
 
