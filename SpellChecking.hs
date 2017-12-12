a89b21fac1f4051e5de220a6868cfcbd5f1c40a0
Anh Dang <aqdang16@earlham.edu>
2017-12-12 16:45:24 -0500

    .

--- SpellChecking.hs
+++ SpellChecking.hs
@@ -18,6 +18,7 @@ subtitution word = [left ++ c:tail right |(left,right) <- splits word, (not . nu
 
 splits :: String -> [(String,String)]
 splits word = zip (inits word) (tails word)
+--
 
 editOnce :: String -> [String]
 editOnce word = (subtitution word) ++ (addition word) ++ (deletion word)
@@ -25,6 +26,7 @@ editOnce word = (subtitution word) ++ (addition word) ++ (deletion word)
 editNextStep :: [String] -> [String]
 editNextStep ls = [ x | e <- ls, x <- (editOnce e) ]
 
+-- file handle functions
 removePunc :: String -> String
 removePunc str = [x | x <- str, not (x `elem` punc)]
 	where punc = ",.?!:;\"\'#$%\n"
@@ -45,24 +47,26 @@ handleInput path = do
 	let split = splitOn " " (toLowerString (internalHandle (removePunc contents)))
 	let ls = [ x | x <- split, not (wordNoLetters x) ]
 	return ls
+--
 
 --Import Dictionary into List
 getDiction :: FilePath -> IO [String]
 getDiction path = do
 	contents <- readFile path
 	return (lines contents)
+--
 
 --SpellCheck
-
 getInterSection10 :: [String] -> [String] -> Set.Set String
 getInterSection10 mis dic = Set.take 10 (Set.intersection (Set.fromList mis) (Set.fromList dic ))
+--
 
---checkFile :: FilePath -> FilePath -> FilePath ->
+
 checkFile dict mis result = do
 	contents <- handleInput mis
 	diction <- getDiction dict
 	--let needFix = [x | x <- contents, not (Set.isSubsetOf (Set.fromList [x]) (Set.fromList diction))]
 	let needFix = [x | x <- contents, not (x `elem` diction)]
-	let fixed = [unwords (x : ":" : (Set.toList ((getInterSection10 (editNextStep (editOnce x)) diction)))) | x <- needFix ]
+	let fixed = [unwords (x : ":" : (Set.toList (getInterSection10 (editOnce x) diction))) | x <- needFix ]
 	writeFile result (unlines fixed)
 	--return fixed
